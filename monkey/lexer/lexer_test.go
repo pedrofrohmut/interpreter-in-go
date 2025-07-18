cf98b185b1d0e15c80b5c639fadeadf65107de99
pedrofrohmut <pedrofrohmut@gmail.com>
2025-07-18 19:02:12 -0300

    Testing for a lexer for an empty string

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -13,12 +13,27 @@ func TestNewLexer(t *testing.T) {
 
 	if lx.input != input {
 		t.Errorf("Expected lexer.input to be %q, but got %q", input, lx.input)
-		return
 	}
-
 	if lx.ch != input[0] {
 		t.Errorf("Expect lexer.ch to be %q, but got %q", input[0], lx.ch)
-		return
+	}
+}
+
+func TestNewLexerEmptyInput(t *testing.T) {
+	input := ""
+	lx := NewLexer(input)
+
+	if lx.position != 0 {
+		t.Errorf("Expected lexer.position to be %q, but got %q", 0, lx.position)
+	}
+	if lx.readPosition >= 1 {
+		t.Errorf("Expected lexer.readPosition to be %q, but got %q", 0, lx.readPosition)
+	}
+	if lx.input != "" {
+		t.Errorf("Expected lexer.input to be %q, but got %q", "", lx.input)
+	}
+	if lx.ch != 0 {
+		t.Errorf("Expected lexer.ch to be %q, but got %q", 0, lx.ch)
 	}
 }
 
