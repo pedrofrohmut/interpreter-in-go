9a43fdd173d9583eab1317fb747c1805c9d57da1
pedrofrohmut <pedrofrohmut@gmail.com>
2025-07-31 16:42:33 -0300

    Rewriting parse, ast and parser test - Initial LetStatements

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -7,55 +7,82 @@
 package ast
 
 import (
-    "bytes"
+    _"bytes"
+    _"strconv"
     "monkey/token"
 )
 
 type Node interface {
-    String() string
+    TokenLiteral() string
+    // String() string
 }
 
 type Statement interface {
     Node
+    statementNode()
+}
+
+type Expression interface {
+    Node
+    expressionNode()
 }
 
 type Program struct {
-    Node
     Statements []Statement
 }
 
 // @Impl
-func (pro *Program) String() string {
-    var out bytes.Buffer
-    for _, stm := range pro.Statements {
-        out.WriteString(stm.String())
-    }
-    return out.String()
+func (this *Program) TokenLiteral() string {
+    if len(this.Statements) == 0 { return "" }
+    return this.Statements[0].TokenLiteral()
 }
 
 func NewProgram() *Program {
-    return &Program { Statements: [] Statement{} }
-}
-
-// TODO: Keeping checking if Identifier needs to be a separated struct for reuse
-type LetStatement struct {
-    Statement
-    Token token.Token
-    Identifier string
-    Expression string
-}
-
-// @Impl
-func (stm *LetStatement) String() string {
-    var out bytes.Buffer
-    out.WriteString(stm.Token.Literal + " ")
-    out.WriteString(stm.Identifier + " = ")
-    if stm.Expression != "" {
-        out.WriteString(stm.Expression)
+    return &Program {
+        Statements: [] Statement{},
     }
-    out.WriteString(";")
-    return out.String()
 }
+
+// // @Impl
+// func (pro *Program) String() string {
+//     var out bytes.Buffer
+//     for _, stm := range pro.Statements {
+//         out.WriteString(stm.String())
+//     }
+//     return out.String()
+// }
+//
+// func NewProgram() *Program {
+//     return &Program { Statements: [] Statement{} }
+// }
+
+type Identifier struct {
+    Token token.Token
+    Value string
+}
+
+func (this *Identifier) expressionNode() {}
+func (this *Identifier) TokenLiteral() string { return this.Token.Literal }
+
+func NewIdentifier(tok token.Token, val string) *Identifier {
+    return &Identifier { Token: tok, Value: val }
+}
+
+// // TODO: Keeping checking if Identifier needs to be a separated struct for reuse
+// type LetStatement struct {
+//     Statement
+//     Token token.Token
+//     Identifier string
+//     Expression string
+// }
+type LetStatement struct {
+    Token token.Token
+    Left *Identifier
+    Right *Expression
+}
+
+func (this *LetStatement) statementNode() {}
+func (this *LetStatement) TokenLiteral() string { return this.Token.Literal }
 
 func NewLetStatement() *LetStatement {
     return &LetStatement {
@@ -63,66 +90,107 @@ func NewLetStatement() *LetStatement {
     }
 }
 
-type ReturnStatement struct {
-    Statement
-    Token token.Token
-    Expression string
-}
-
-// @Impl
-func (stm *ReturnStatement) String() string {
-    var out bytes.Buffer
-    out.WriteString(stm.Token.Literal + " ")
-    if stm.Expression != "" {
-        out.WriteString(stm.Expression)
-    }
-    out.WriteString(";")
-    return out.String()
-}
-
-func NewReturnStatement() *ReturnStatement {
-    return &ReturnStatement {
-        Token: token.NewTokenStr(token.RETURN, "return"),
-    }
-}
-
-// We are having Expression Statements because in monkey you can have expressions
-// as statements. Exp: 5 * 5 + 3;. So it is needed to have it as statement here
-type ExpressionStatement struct {
-    Statement
-    Token token.Token // The first token of the expression
-    Expression string
-}
-
-// @Impl
-func (stm *ExpressionStatement) String() string {
-    var out bytes.Buffer
-    if stm.Expression != "" {
-        out.WriteString(stm.Expression)
-    }
-    // TODO: Check if should just return an empty string or ; is fine
-    out.WriteString(";")
-    return out.String()
-}
-
-func NewExpressionStatement(first token.Token) *ExpressionStatement {
-    return &ExpressionStatement { Token: first }
-}
-
-type IntegerLiteral struct {
-    Node
-    Token token.Token
-    Value int64
-}
-
-func NewIntegerLiteral(val int64) *IntegerLiteral {
-    return &IntegerLiteral {
-        Token: token.Token { Type: token.INT, Literal: string(val) },
-        Value: val,
-    }
-}
-
-// @Impl
-func (lit *IntegerLiteral) String() string {
-    return lit.Token.Literal
-}
+//
+// // @Impl
+// func (stm *LetStatement) String() string {
+//     var out bytes.Buffer
+//     out.WriteString(stm.Token.Literal + " ")
+//     out.WriteString(stm.Identifier + " = ")
+//     if stm.Expression != "" {
+//         out.WriteString(stm.Expression)
+//     }
+//     out.WriteString(";")
+//     return out.String()
+// }
+//
+// func NewLetStatement() *LetStatement {
+//     return &LetStatement {
+//         Token: token.NewTokenStr(token.LET, "let"),
+//     }
+// }
+//
+// type ReturnStatement struct {
+//     Statement
+//     Token token.Token
+//     Expression string
+// }
+//
+// // @Impl
+// func (stm *ReturnStatement) String() string {
+//     var out bytes.Buffer
+//     out.WriteString(stm.Token.Literal + " ")
+//     if stm.Expression != "" {
+//         out.WriteString(stm.Expression)
+//     }
+//     out.WriteString(";")
+//     return out.String()
+// }
+//
+// func NewReturnStatement() *ReturnStatement {
+//     return &ReturnStatement {
+//         Token: token.NewTokenStr(token.RETURN, "return"),
+//     }
+// }
+//
+// // We are having Expression Statements because in monkey you can have expressions
+// // as statements. Exp: 5 * 5 + 3;. So it is needed to have it as statement here
+// type ExpressionStatement struct {
+//     Token token.Token // The first token of the expression
+//     Expression Expression
+// }
+//
+// func (this *ExpressionStatement) statementNode() {}
+//
+// // @Impl
+// func (stm *ExpressionStatement) String() string {
+//     var out bytes.Buffer
+//     if stm.Expression != "" {
+//         out.WriteString(stm.Expression)
+//     }
+//     // TODO: Check if should just return an empty string or ; is fine
+//     out.WriteString(";")
+//     return out.String()
+// }
+//
+// func NewExpressionStatement(first token.Token) *ExpressionStatement {
+//     return &ExpressionStatement { Token: first }
+// }
+//
+// type IntegerLiteral struct {
+//     Node
+//     Token token.Token
+//     Value int64
+// }
+//
+// func NewIntegerLiteral(val int64) *IntegerLiteral {
+//     return &IntegerLiteral {
+//         Token: token.Token { Type: token.INT, Literal: strconv.FormatInt(val, 10) },
+//         Value: val,
+//     }
+// }
+//
+// // @Impl
+// func (lit *IntegerLiteral) String() string {
+//     return lit.Token.Literal
+// }
+//
+// type PrefixExpression struct {
+//     Token token.Token // The prefix token
+//     Operator string
+//     Right string
+// }
+//
+// func (this *PrefixExpression) expressionNode() {}
+//
+// func (exp *PrefixExpression) String() string {
+//     var out bytes.Buffer
+//     out.WriteString("(")
+//     out.WriteString(exp.Operator)
+//     out.WriteString(exp.Right)
+//     out.WriteString(")")
+//     return out.String()
+// }
+//
+// func NewPrefixExpression() *PrefixExpression {
+//     return &PrefixExpression {}
+// }
--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -9,11 +9,12 @@ package parser
 
 import (
     "fmt"
-    "strconv"
     "monkey/ast"
     "monkey/lexer"
     "monkey/token"
     "monkey/utils"
+    _ "monkey/utils"
+    _ "strconv"
 )
 
 const (
@@ -29,182 +30,289 @@ const (
     CALL        // myFunction(X)
 )
 
-// TODO: On book the all strings here are ast.Expression
 type (
-    PrefixParseFn func() string
-    InfixParseFn func(string) string
+    PrefixParseFn func() ast.Expression
+    InfixParseFn func(ast.Expression) ast.Expression
 )
 
 type Parser struct {
     lex *lexer.Lexer
     errors []string
+    currToken token.Token
+    peekToken token.Token
     prefixParseFns map[token.TokenType]PrefixParseFn
     infixParseFns map[token.TokenType]InfixParseFn
-
-    // My custom variables to check tokens
-    // TODO: check if it can be replaced to a single reference token like
-    // 'currToken' instead of an array for token (lower memory footprint)
-    tokens []token.Token
 }
 
 func NewParser(lex *lexer.Lexer) *Parser {
     par := &Parser {
         lex: lex,
         errors: []string {},
-        tokens: []token.Token {},
     }
-    par.prefixParseFns = make(map[token.TokenType]PrefixParseFn)
-    par.addPrefixFn(token.IDENT, par.parseIdentifier)
-    par.addPrefixFn(token.INT, par.parseIntegerLiteral)
+    par.currToken = lex.GetNextToken()
+    par.peekToken = lex.GetNextToken()
     return par
 }
 
-func (par *Parser) parseIdentifier() string {
-    return par.GetCurrToken().Literal
+func (this *Parser) Errors() []string {
+    return this.errors
 }
 
-func (par *Parser) parseIntegerLiteral() string {
-    curr := par.GetCurrToken()
-    _, err := strconv.ParseInt(curr.Literal, 0, 64)
-    if err != nil {
-        msg := fmt.Sprintf("Could not parse %q as integer", curr.Literal)
-        par.errors = append(par.errors, msg)
-        return ""
-    }
-    return curr.Literal
+func (this *Parser) addTokenError(check token.TokenType, expected token.TokenType) {
+    msg := fmt.Sprintf("Expected token type to be '%s' but got '%s' instead", expected, check)
+    this.errors = append(this.errors, msg)
 }
 
-func (par *Parser) GetCurrToken() token.Token {
-    if len(par.tokens) == 0 {
-        return token.Token {}
-    }
-    return par.tokens[len(par.tokens) - 1]
+func (this *Parser) nextToken() {
+    this.currToken = this.peekToken
+    this.peekToken = this.lex.GetNextToken()
 }
 
-func (par *Parser) GetNextToken() token.Token {
-    tok := par.lex.GetNextToken()
-
-    // Dont add extra EOF tokens at the end
-    if tok.Type == token.EOF && par.GetCurrToken().Type == token.EOF {
-        return tok
-    }
-
-    par.tokens = append(par.tokens, tok)
-    return tok
-}
-
-// TODO: check if this fn is necessary
-func (par *Parser) addPrefixFn(typ token.TokenType, fn PrefixParseFn) {
-    par.prefixParseFns[typ] = fn
-}
-
-// TODO: check if this fn is necessary
-func (par *Parser) addInfixFn(typ token.TokenType, fn InfixParseFn) {
-    par.infixParseFns[typ] = fn
-}
-
-func (par *Parser) addTokenError(expected token.TokenType, tok token.Token) {
-    err := fmt.Sprintf("Expected token type to be %s but got %s instead.", expected, tok.Type)
-    par.errors = append(par.errors, err)
-}
-
-func (par *Parser) parseLetStatement() *ast.LetStatement {
+func (this *Parser) parseLetStatement() *ast.LetStatement {
     stm := ast.NewLetStatement()
-    hasErr := false
+    hasError := false
 
-    // Check for identifier
-    tok := par.GetNextToken()
-    if tok.Type != token.IDENT {
-        par.addTokenError(token.IDENT, tok)
-        hasErr = true
+    // Check for indentifier
+    this.nextToken()
+    if this.currToken.Type != token.IDENT {
+        this.addTokenError(this.currToken.Type, token.IDENT)
+        hasError = true
     }
-    stm.Identifier = tok.Literal
+    stm.Left = ast.NewIdentifier(this.currToken, this.currToken.Literal)
 
-    // Check for assign item
-    if !hasErr {
-        tok = par.GetNextToken()
+    // Check for the assign operator
+    if !hasError {
+        this.nextToken()
     }
-    if tok.Type != token.ASSIGN {
-        par.addTokenError(token.ASSIGN, tok)
-        hasErr = true
-    }
-
-    // TODO: Check out how to parse Expression from letStm
-
-    // Advance tokens until find a semicolon
-    for tok.Type != token.SEMICOLON {
-        tok = par.GetNextToken()
+    if this.currToken.Type != token.ASSIGN {
+        this.addTokenError(this.currToken.Type, token.ASSIGN)
+        hasError = true
     }
 
-    if hasErr { return nil }
+    // TODO: Skipping the expression until find a semicolon (ParseExpression)
+    for this.currToken.Type != token.SEMICOLON { this.nextToken() }
+
+    if hasError { return nil }
 
     return stm
 }
 
-func (par *Parser) parseReturnStatement() *ast.ReturnStatement {
-    stm := ast.NewReturnStatement()
-    var tok token.Token
-
-    // TODO: Check out how to parse Expression from ReturnStatement
-
-    // Advance tokens until find a semicolon
-    for tok.Type != token.SEMICOLON {
-        tok = par.GetNextToken()
-    }
-
-    return stm
-}
-
-func (par *Parser) parseExpression(precedence int) string {
-    curr := par.GetCurrToken()
-    prefix := par.prefixParseFns[curr.Type]
-    if utils.IsNill(prefix) { return "" }
-    return prefix()
-}
-
-func (par *Parser) parseExpressionStatement() *ast.ExpressionStatement {
-    parsedExp := par.parseExpression(LOWEST)
-    if parsedExp == "" { return nil }
-
-    stm := ast.NewExpressionStatement(par.GetCurrToken())
-    stm.Expression = parsedExp
-
-    // To skip semicolons so you can use no semicolon expressions on the REPL
-    if par.GetCurrToken().Type == token.SEMICOLON {
-        par.GetNextToken()
-    }
-
-    return stm
-}
-
-func (par *Parser) parseStatement() ast.Statement {
-    switch par.GetCurrToken().Type {
+func (this *Parser) parseStatement() ast.Statement {
+    switch this.currToken.Type {
     case token.LET:
-        return par.parseLetStatement()
-    case token.RETURN:
-        return par.parseReturnStatement()
+        return this.parseLetStatement()
     default:
-        return par.parseExpressionStatement()
+        return nil
     }
 }
 
-func (par *Parser) ParseProgram() *ast.Program {
+func (this *Parser) ParseProgram() *ast.Program {
     pro := ast.NewProgram()
-
-    tok := par.GetNextToken()
-    for tok.Type != token.EOF {
-        stm := par.parseStatement()
+    for this.currToken.Type != token.EOF {
+        stm := this.parseStatement()
         if !utils.IsNill(stm) {
             pro.Statements = append(pro.Statements, stm)
         }
-        tok = par.GetNextToken()
+        this.nextToken()
     }
-
     return pro
 }
 
-func (par *Parser) PrintParserErrors() {
-    for i, x := range par.errors {
-        fmt.Printf("[%d] Parser error: %s\n", i, x)
-    }
-}
+//
+// // TODO: On book the all strings here are ast.Expression
+// type (
+//     PrefixParseFn func() ast.Expression
+//     InfixParseFn func(ast.Expression) ast.Expression
+// )
+//
+// type Parser struct {
+//     lex *lexer.Lexer
+//     errors []string
+//     prefixParseFns map[token.TokenType]PrefixParseFn
+//     infixParseFns map[token.TokenType]InfixParseFn
+//
+//     // My custom variables to check tokens
+//     // TODO: check if it can be replaced to a single reference token like
+//     // 'currToken' instead of an array for token (lower memory footprint)
+//     tokens []token.Token
+// }
+//
+// func NewParser(lex *lexer.Lexer) *Parser {
+//     par := &Parser {
+//         lex: lex,
+//         errors: []string {},
+//         tokens: []token.Token {},
+//     }
+//     par.prefixParseFns = make(map[token.TokenType]PrefixParseFn)
+//     par.addPrefixFn(token.IDENT, par.parseIdentifier)
+//     par.addPrefixFn(token.INT, par.parseIntegerLiteral)
+//     par.addPrefixFn(token.BANG, par.parsePrefixExpression)
+//     par.addPrefixFn(token.MINUS, par.parsePrefixExpression)
+//     return par
+// }
+//
+// func (par *Parser) parseIdentifier() ast.Expression {
+//     return par.GetCurrToken().Literal
+// }
+//
+// func (par *Parser) parseIntegerLiteral() ast.Expression {
+//     curr := par.GetCurrToken()
+//     _, err := strconv.ParseInt(curr.Literal, 0, 64)
+//     if err != nil {
+//         msg := fmt.Sprintf("Could not parse %q as integer", curr.Literal)
+//         par.errors = append(par.errors, msg)
+//         return nil
+//     }
+//     // return curr.Literal
+// }
+//
+// // TODO: This is wrong somehow
+// func (par *Parser) parsePrefixExpression() ast.Expression {
+//     curr := par.GetCurrToken()
+//     exp := &ast.PrefixExpression { Token: curr, Operator: curr.Literal }
+//     exp.Right = par.parseExpression(PREFIX)
+//     return exp
+// }
+//
+// func (par *Parser) GetCurrToken() token.Token {
+//     if len(par.tokens) == 0 {
+//         return token.Token {}
+//     }
+//     return par.tokens[len(par.tokens) - 1]
+// }
+//
+// func (par *Parser) GetNextToken() token.Token {
+//     tok := par.lex.GetNextToken()
+//
+//     // Dont add extra EOF tokens at the end
+//     if tok.Type == token.EOF && par.GetCurrToken().Type == token.EOF {
+//         return tok
+//     }
+//
+//     par.tokens = append(par.tokens, tok)
+//     return tok
+// }
+//
+// // TODO: check if this fn is necessary
+// func (par *Parser) addPrefixFn(typ token.TokenType, fn PrefixParseFn) {
+//     par.prefixParseFns[typ] = fn
+// }
+//
+// // TODO: check if this fn is necessary
+// func (par *Parser) addInfixFn(typ token.TokenType, fn InfixParseFn) {
+//     par.infixParseFns[typ] = fn
+// }
+//
+// func (par *Parser) addTokenError(expected token.TokenType, tok token.Token) {
+//     err := fmt.Sprintf("Expected token type to be %s but got %s instead.", expected, tok.Type)
+//     par.errors = append(par.errors, err)
+// }
+//
+// func (par *Parser) parseLetStatement() *ast.LetStatement {
+//     stm := ast.NewLetStatement()
+//     hasErr := false
+//
+//     // Check for identifier
+//     tok := par.GetNextToken()
+//     if tok.Type != token.IDENT {
+//         par.addTokenError(token.IDENT, tok)
+//         hasErr = true
+//     }
+//     stm.Identifier = tok.Literal
+//
+//     // Check for assign item
+//     if !hasErr {
+//         tok = par.GetNextToken()
+//     }
+//     if tok.Type != token.ASSIGN {
+//         par.addTokenError(token.ASSIGN, tok)
+//         hasErr = true
+//     }
+//
+//     // TODO: Check out how to parse Expression from letStm
+//
+//     // Advance tokens until find a semicolon
+//     for tok.Type != token.SEMICOLON {
+//         tok = par.GetNextToken()
+//     }
+//
+//     if hasErr { return nil }
+//
+//     return stm
+// }
+//
+// func (par *Parser) parseReturnStatement() *ast.ReturnStatement {
+//     stm := ast.NewReturnStatement()
+//     var tok token.Token
+//
+//     // TODO: Check out how to parse Expression from ReturnStatement
+//
+//     // Advance tokens until find a semicolon
+//     for tok.Type != token.SEMICOLON {
+//         tok = par.GetNextToken()
+//     }
+//
+//     return stm
+// }
+//
+// func (par *Parser) parseExpression(precedence int) string {
+//     curr := par.GetCurrToken()
+//     prefix := par.prefixParseFns[curr.Type]
+//
+//     fmt.Printf("Parse Expression Curr: %s, \tPrefix? %t\n", curr.Type, utils.IsNill(prefix))
+//
+//     if utils.IsNill(prefix) {
+//         msg := fmt.Sprintf("No prefix parse function for %s found", curr.Type)
+//         par.errors = append(par.errors, msg)
+//         return ""
+//     }
+//
+//     return prefix()
+// }
+//
+// func (par *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+//     parsedExp := par.parseExpression(LOWEST)
+//     if parsedExp == "" { return nil }
+//
+//     stm := ast.NewExpressionStatement(par.GetCurrToken())
+//     stm.Expression = parsedExp
+//
+//     // To skip semicolons so you can use no semicolon expressions on the REPL
+//     if par.GetCurrToken().Type == token.SEMICOLON {
+//         par.GetNextToken()
+//     }
+//
+//     return stm
+// }
+//
+// func (par *Parser) parseStatement() ast.Statement {
+//     switch par.GetCurrToken().Type {
+//     case token.LET:
+//         return par.parseLetStatement()
+//     case token.RETURN:
+//         return par.parseReturnStatement()
+//     default:
+//         return par.parseExpressionStatement()
+//     }
+// }
+//
+// func (par *Parser) ParseProgram() *ast.Program {
+//     pro := ast.NewProgram()
+//
+//     tok := par.GetNextToken()
+//     for tok.Type != token.EOF {
+//         stm := par.parseStatement()
+//         if !utils.IsNill(stm) {
+//             pro.Statements = append(pro.Statements, stm)
+//         }
+//         tok = par.GetNextToken()
+//     }
+//
+//     return pro
+// }
+//
+// func (par *Parser) PrintParserErrors() {
+//     for i, x := range par.errors {
+//         fmt.Printf("[%d] Parser error: %s\n", i, x)
+//     }
+// }
